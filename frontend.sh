58739b7d89a4784351f9ee0583b4276c2edf7f0a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-18 00:12:41 +0530

    new-file

--- frontend.sh
+++ frontend.sh
@@ -10,8 +10,8 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
+   if [ $1 -ne 0 ]
+   then
         echo -e "$2...$R FAILURE $N"
         exit 1
     else
@@ -21,36 +21,34 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run the script with root access"
-    exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are super user"
+    echo "You are super user."
 fi
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enable nginx"
-
-nginx -t &>>$LOGFILE
-VALIDATE $? "Nginx configuration test"
+VALIDATE $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "Removing existing contenet"
+VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading frontend code"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting code"
+VALIDATE $? "Extracting frontend code"
 
+#check your repo and path
 cp /home/ec2-user/expense-project-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copying backend servive"
+VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restart nginx"
+VALIDATE $? "Restarting nginx"
