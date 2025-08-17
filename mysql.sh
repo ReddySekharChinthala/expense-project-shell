b8764f3d21a09ffe09e4069117313db86c3b077f
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-17 12:11:29 +0530

    new-file

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is..$R FAILURE $N"
+        exit 1
+    else
+        echo "$2 is..$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run script with root access"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+dnf install mysql-server -y &>>LOGFILE
+VALIDATE $? "Installing MySQL_Server"
+
+systemctl enable mysqld &>>LOGFILE
+VALIDATE $? "Enabling MySQL_Server"
+
+systemctl start mysqld &>>LOGFILE
+VALIDATE $? "Starting MySQL_Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up root password"
