42d98b9d7441ef999db3a8d99eb62134044dbbf8
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-17 20:00:57 +0530

    new-file

--- backend.sh
+++ backend.sh
@@ -9,26 +9,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0"
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo "$2 is...$R FAILURE $N"
-#         exit 1
-#     else
-#         echo "$2 is...$G SUCCESS $N"
-#     fi
-
-
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE$N"
         exit 1
     else
         echo -e "$2...$G SUCCESS $N"
     fi
 }
-
 
 if [ $? -ne 0 ]
 then
