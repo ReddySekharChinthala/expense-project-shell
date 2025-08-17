604d02b96da0c071ecdb2c7ca2db71d415b4a97c
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-17 20:03:11 +0530

    new-file

--- backend.sh
+++ backend.sh
@@ -7,12 +7,12 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2...$R FAILURE$N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
         echo -e "$2...$G SUCCESS $N"
